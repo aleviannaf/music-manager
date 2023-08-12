f7e4cb1112e9e75b9002a24e285f598ee4953a36
aleviannaf <alexandrevianna511@gmail.com>
2023-08-11 23:02:35 -0400

    feat(router): protect router to login

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import "express-async-errors";
 import express, { Application, json } from "express";
 import middlewares from "./middlewares";
-import { userRouter } from "./routers";
+import { sessionRouter, userRouter } from "./routers";
 
 const app: Application = express();
 app.use(json());
 
 app.use("/users", userRouter);
+app.use("/login", sessionRouter)
 
 app.use(middlewares.handleErrors);
 
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,3 +1,3 @@
 import userRouter from "./user.router";
-
-export { userRouter };
+import sessionRouter from "./session.router";
+export { userRouter, sessionRouter };
