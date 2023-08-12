ca7234581f92b995934eb022b0a84224cecbedaa
aleviannaf <alexandrevianna511@gmail.com>
2023-08-12 16:14:46 -0400

    update: update in index add new middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,10 +2,14 @@ import handleErrors from "./handleErrors";
 import validateIdExists from "./validateIdExists.middleware";
 import validateUsernameExists from "./validateUsernameExists.middleware";
 import validateBody from "./validateBody.middleware";
+import verifyToken from "./verifyToken.middleware";
+import verifyUserPermisson from "./verifyUserPermission.middleware";
 
 export default {
   handleErrors,
   validateIdExists,
   validateUsernameExists,
   validateBody,
+  verifyToken,
+  verifyUserPermisson
 };
