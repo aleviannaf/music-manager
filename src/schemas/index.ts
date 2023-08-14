cc682af02cbed7be0feda704f2b8eef973b77ec1
aleviannaf <alexandrevianna511@gmail.com>
2023-08-14 16:36:57 -0400

    update: added new schemas in index

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,4 +1,14 @@
 import { user, userCreate, userUpdate, userRead, userReturn } from "./user.schema";
-import { sessionCreate } from "./session.schema";
+import { sessionCreate } from "./session.schema"
+import { playlist, playlistCreate } from "./playlist.schemas"
 
-export { user, userCreate, userUpdate, userRead, sessionCreate, userReturn};
+export {
+    user,
+    userCreate,
+    userUpdate,
+    userRead,
+    sessionCreate,
+    userReturn,
+    playlist,
+    playlistCreate,
+};
