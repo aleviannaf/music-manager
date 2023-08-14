a77c22913c0740700aab086a33971ceec3dd1e11
aleviannaf <alexandrevianna511@gmail.com>
2023-08-14 16:36:13 -0400

    feat (schemas): new schemas to playlist and playlistCreate

---
+++ src/schemas/playlist.schemas.ts
@@ -0,0 +1,18 @@
+import { z } from "zod"
+
+const playlist = z.object({
+    id: z.number().positive(),
+    name: z.string().max(100),
+    privacy: z.string().max(20),
+    description: z.string().nullable(),
+    createdAt: z.date(),
+    userID: z.number().positive(),
+})
+
+const playlistCreate = playlist.omit({
+    id: true,
+    createdAt: true,
+    userID: true
+})
+
+export { playlist, playlistCreate }
