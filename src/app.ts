c2e6056399f3e996061333a7e6b9d9316a3e24c3
aleviannaf <alexandrevianna511@gmail.com>
2023-08-15 21:32:58 -0400

    update (added router): update in app to add router playlist

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
 import "express-async-errors";
 import express, { Application, json } from "express";
 import middlewares from "./middlewares";
-import { sessionRouter, userRouter } from "./routers";
+import { playlistRouter, sessionRouter, userRouter } from "./routers";
 
-const app: Application = express();
+const app: Application = express()
 app.use(json());
 
-app.use("/users", userRouter);
+app.use("/users", userRouter)
 app.use("/login", sessionRouter)
+app.use("/playlists", playlistRouter)
 
-app.use(middlewares.handleErrors);
+app.use(middlewares.handleErrors)
 
 export default app;
