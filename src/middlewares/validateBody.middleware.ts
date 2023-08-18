26714b048bfc50a29e8e6a3c6b86e38f5a2dcbba
aleviannaf <alexandrevianna511@gmail.com>
2023-08-18 14:40:03 -0400

    update: update in middleware to receive z.ZodTypeAny

--- src/middlewares/validateBody.middleware.ts
+++ src/middlewares/validateBody.middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { z } from "zod";
 
 const validateBody =
-  (schema: z.AnyZodObject) =>
+  (schema: z.ZodTypeAny) =>
   (req: Request, res: Response, next: NextFunction): void => {
     const validated = schema.parse(req.body);
     res.locals = { ...res.locals, validated };
