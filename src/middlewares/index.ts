6a9270d116893165cf3f3eebaff478aa2694f1bf
aleviannaf <alexandrevianna511@gmail.com>
2023-08-18 14:42:36 -0400

    update: update in index.ts of the middleware to add validatedAdmin middleare

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -4,6 +4,7 @@ import validateUsernameExists from "./validateUsernameExists.middleware";
 import validateBody from "./validateBody.middleware";
 import verifyToken from "./verifyToken.middleware";
 import verifyUserPermisson from "./verifyUserPermission.middleware";
+import validatedAdmin from "./validatedAdmin.middleware";
 
 export default {
   handleErrors,
@@ -11,5 +12,6 @@ export default {
   validateUsernameExists,
   validateBody,
   verifyToken,
-  verifyUserPermisson
+  verifyUserPermisson,
+  validatedAdmin
 };
