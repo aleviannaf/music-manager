cba8dde32148c07899c8c4f26c1f315353ca8acb
aleviannaf <alexandrevianna511@gmail.com>
2023-08-17 20:59:43 -0400

    feat (controller): added controller to return all playlists

--- src/controllers/playlist.controllers.ts
+++ src/controllers/playlist.controllers.ts
@@ -3,9 +3,21 @@ import { Playlist } from "../interfaces"
 import { playlistServices } from "../services"
 
 const create = async (req: Request, res: Response): Promise<Response> => {
-    const playlist: Playlist = await playlistServices.create(res.locals.validated, res.locals.decoded.sub)
+    const playlist: Playlist = await playlistServices.create(
+        res.locals.validated, 
+        res.locals.decoded.sub
+    )
     
     return res.status(201).json(playlist)
 }
 
-export default { create }
+const read = async (req: Request, res: Response): Promise<Response> => {
+    const playlist: Playlist[] = await playlistServices.read(
+        res.locals.decoded.admin
+    )
+    
+    return res.status(201).json(playlist)
+}
+
+
+export default { create, read }
