db348db2d8b58af0119384852f3af3081b1e6106
aleviannaf <alexandrevianna511@gmail.com>
2023-09-21 15:45:27 -0400

    update: centralize verify ids

--- src/middlewares/validateIdExists.middleware.ts
+++ src/middlewares/validateIdExists.middleware.ts
@@ -2,26 +2,45 @@ import { NextFunction, Request, Response } from "express";
 import { UserResult } from "../interfaces";
 import { client } from "../database";
 import { AppError } from "../errors";
+import format from "pg-format";
 
-const validateIdExists = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  const { userId } = req.params;
+type BodyOrParams = "params" | "body"
 
-  const query: UserResult = await client.query(
-    'SELECT * FROM "users" WHERE "id" = $1',
-    [userId]
-  );
+const validateIdExists =
+  (
+    bodyOrParams: BodyOrParams,
+    idKeyName: string,
+    tbName: string,
+    errorMsg: string
+  ) =>
+    async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+      /**De onde vem o id?
+      *  1. Corpo da requisição.
+      *  2. Parâmetros de rota.
+      * Quais são os pontos dinâmicos desse middleware?
+      *  1. Mensagem de erro.
+      *  2. Qual a tabela que vai ser feita a query.
+      *  3. Qual o nome do id.
+      */
+      const id = req[bodyOrParams][idKeyName]
 
-  if (query.rowCount === 0) {
-    throw new AppError("User not found", 404);
-  }
+      const queryFormat: string = format(
+        `SELECT * FROM %I WHERE "id" = $1`,
+        tbName
+      )
 
-  res.locals = { ...res.locals, foundUser: query.rows[0] };
+      const queryResult: UserResult = await client.query(
+        '',
+        [id]
+      );
 
-  return next();
-};
+      if (queryResult.rowCount === 0) {
+        throw new AppError(errorMsg, 404);
+      }
+
+      res.locals = { ...res.locals, foundUser: queryResult.rows[0] };
+
+      return next();
+    };
 
 export default validateIdExists;
